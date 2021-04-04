544782bc4ee095c9ac059fea96cd18d1023d4fdf
souhub <ace0804sou@gmail.com>
2021-04-04 14:17:06 +0900

    Updated epsirons to epsilons

--- pkg/model/trainingg.go
+++ pkg/model/trainingg.go
@@ -4,6 +4,6 @@ type TrainingData struct {
 	ID           int    `scheme:"id"`
 	ActressesIDs string `schema:"actresses_ids"` // gorrila/scheme に必要
 	States       string `scheme:"states"`
-	Epsilons     string `scheme:"epsirons"`
+	Epsilons     string `scheme:"epsions"`
 	SelectedID   int    `scheme:"selected_id"`
 }
