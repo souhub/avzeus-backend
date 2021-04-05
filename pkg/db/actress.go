c39f2d2ef2065866e063737e0a4075993b938300
souhub <ace0804sou@gmail.com>
2021-04-05 23:26:43 +0900

    Added isExist func

--- pkg/db/actress.go
+++ pkg/db/actress.go
@@ -1,13 +1,17 @@
 package db
 
 import (
+	"bufio"
 	"database/sql"
 	"errors"
 	"log"
+	"os"
+	"strings"
 
 	"github.com/souhub/avzeus-backend/pkg/model"
 )
 
+// Actressを全県取得し、構造体に入れて返す
 func FetchActresses() (actresses model.Actresses) {
 	rows := fetchActressesRows()
 	for rows.Next() {
@@ -21,6 +25,7 @@ func FetchActresses() (actresses model.Actresses) {
 	return actresses
 }
 
+// AIから受け取ったrecommended_actresses_idsをもとに、レコードを取得し、構造体に入れて返す
 func FetchRecommendedActresses(ids []int) (recommendedActresses model.Actresses, err error) {
 	for _, id := range ids {
 		row, err := fetchActressRow(id)
@@ -37,6 +42,7 @@ func FetchRecommendedActresses(ids []int) (recommendedActresses model.Actresses,
 	return recommendedActresses, err
 }
 
+// Actressテーブルからレコードを全取得
 func fetchActressesRows() *sql.Rows {
 	query := `SELECT * FROM actresses`
 	rows, err := dbCon.Query(query)
@@ -50,6 +56,7 @@ func fetchActressesRows() *sql.Rows {
 	return rows
 }
 
+// 任意のレコードを１つActressテーブルから取得
 func fetchActressRow(id int) (row *sql.Row, err error) {
 	query := `SELECT * FROM actresses WHERE id=?`
 	row = dbCon.QueryRow(query, id)
@@ -59,4 +66,70 @@ func fetchActressRow(id int) (row *sql.Row, err error) {
 		}
 	}
 	return row, err
+}
+
+// Actressテーブルの初期データ挿入
+func insertActresses(names []string) error {
+	// トランザクション開始
+	tx, err := dbCon.Begin()
+	if err != nil {
+		return err
+	}
+
+	// クエリ定義
+	query := `INSERT INTO actresses (name) VALUES (?)`
+
+	// 141人分トランザクションでInsertする
+	for _, name := range names {
+		tx.Exec(query, name)
+	}
+
+	// コミット
+	err = tx.Commit()
+	// エラー発生したらロールバックする
+	if err != nil {
+		if err = tx.Rollback(); err != nil {
+			return err
+		}
+		return err
+	} else {
+		return nil
+	}
+}
+
+// actress_id.txtから名前を配列にしたものを取得
+func getActressesDataFromText(fileName string) (names []string, err error) {
+	fp, err := os.Open(fileName)
+	if err != nil {
+		return names, err
+	}
+	defer fp.Close()
+
+	scanner := bufio.NewScanner(fp)
+	for scanner.Scan() {
+		line := scanner.Text()
+		actress := strings.Split(line, ":")
+		name := actress[1]
+		names = append(names, name)
+	}
+	return names, nil
+}
+
+// actressテーブルにレコードが存在するか否か
+func isExist() bool {
+	query := `SELECT COUNT(*) FROM actress`
+	rows, err := dbCon.Query(query)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	var count int
+	for rows.Next() {
+		if err := rows.Scan(&count); err != nil {
+			log.Fatal(err)
+		}
+	}
+	if count == 0 {
+		return false
+	}
+	return true
 }
--- pkg/db/init.go
+++ pkg/db/init.go
@@ -1,12 +1,10 @@
 package db
 
 import (
-	"bufio"
 	"database/sql"
 	"fmt"
 	"log"
 	"os"
-	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -16,15 +14,19 @@ var dbCon *sql.DB
 func init() {
 	// DBとBackendを接続
 	dbCon = NewDB()
-	// actressテーブルにデータ挿入
-	actressesFile := "./pkg/db/actress_id.txt"
-	names, err := getActressesDataFromText(actressesFile)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	err = insertActresses(names)
-	if err != nil {
-		log.Fatalln(err)
+	// actressレコードが存在しなければデータを挿入する
+	isExist := isExist()
+	if !isExist {
+		// actressテーブルにデータ挿入
+		actressesFile := "./pkg/db/actress_id.txt"
+		names, err := getActressesDataFromText(actressesFile)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		err = insertActresses(names)
+		if err != nil {
+			log.Fatalln(err)
+		}
 	}
 }
 
@@ -41,51 +43,4 @@ func NewDB() *sql.DB {
 		log.Fatal(err)
 	}
 	return dbCon
-}
-
-// Actressテーブルの初期データ挿入
-func insertActresses(names []string) error {
-	// トランザクション開始
-	tx, err := dbCon.Begin()
-	if err != nil {
-		return err
-	}
-
-	// クエリ定義
-	query := `INSERT INTO actresses (name) VALUES (?)`
-
-	// 141人分トランザクションでInsertする
-	for _, name := range names {
-		tx.Exec(query, name)
-	}
-
-	// コミット
-	err = tx.Commit()
-	// エラー発生したらロールバックする
-	if err != nil {
-		if err = tx.Rollback(); err != nil {
-			return err
-		}
-		return err
-	} else {
-		return nil
-	}
-}
-
-// actress_id.txtから名前を配列にしたものを取得
-func getActressesDataFromText(fileName string) (names []string, err error) {
-	fp, err := os.Open(fileName)
-	if err != nil {
-		return names, err
-	}
-	defer fp.Close()
-
-	scanner := bufio.NewScanner(fp)
-	for scanner.Scan() {
-		line := scanner.Text()
-		actress := strings.Split(line, ":")
-		name := actress[1]
-		names = append(names, name)
-	}
-	return names, nil
 }
