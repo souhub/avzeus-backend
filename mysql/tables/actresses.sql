3c1816785b12bf8981136e88fcec2ef9a2e40e74
souhub <ace0804sou@gmail.com>
2021-04-05 18:18:58 +0900

    Updated recommendation handler

--- mysql/tables/actresses.sql
+++ mysql/tables/actresses.sql
@@ -4,7 +4,6 @@ CREATE TABLE IF NOT EXISTS actresses
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(255),
     image_path VARCHAR(255),
-    vector VARCHAR(1000),
     PRIMARY KEY(id)
 );
 
@@ -12,142 +11,120 @@ CREATE TABLE IF NOT EXISTS actresses
 INSERT INTO actresses
 (
     name,
-    image_path,
-    vector
+    image_path
 )
 VALUES
 (
     '吉岡ひより',
-    'https://storage.googleapis.com/avzeus-dev/actresses/%E6%98%8E%E6%97%A5%E8%8A%B1%E3%82%AD%E3%83%A9%E3%83%A9.jpeg',
-    '[3,6,77,83,2,44,78,32,1,678,21,5]'
+    'https://storage.googleapis.com/avzeus-dev/actresses/%E6%98%8E%E6%97%A5%E8%8A%B1%E3%82%AD%E3%83%A9%E3%83%A9.jpeg'
 );
 
 INSERT INTO actresses
 (
     name,
-    image_path,
-    vector
+    image_path
 )
 VALUES
 (
     '白石茉莉奈',
-    'https://storage.googleapis.com/avzeus-dev/actresses/%E6%B7%B1%E7%94%B0%E3%81%88%E3%81%84%E3%81%BF.jpeg',
-    '[3,6,77,83,2,44,78,32,1,678,21,5]'
+    'https://storage.googleapis.com/avzeus-dev/actresses/%E6%B7%B1%E7%94%B0%E3%81%88%E3%81%84%E3%81%BF.jpeg'
 );
 
 INSERT INTO actresses
 (
     name,
-    image_path,
-    vector
+    image_path
 )
 VALUES
 (
     '七沢みあ',
-    'https://storage.googleapis.com/avzeus-dev/actresses/%E6%A9%8B%E6%9C%AC%E3%81%82%E3%82%8A%E3%81%AA.jpeg',
-    '[6,33,22,67,887,22,4,7,89,8]'
+    'https://storage.googleapis.com/avzeus-dev/actresses/%E6%A9%8B%E6%9C%AC%E3%81%82%E3%82%8A%E3%81%AA.jpeg'
 );
 
 INSERT INTO actresses
 (
     name,
-    image_path,
-    vector
+    image_path
 )
 VALUES
 (
     '結城るみな',
-    'https://storage.googleapis.com/avzeus-dev/actresses/%E6%A9%8B%E6%9C%AC%E3%81%82%E3%82%8A%E3%81%AA.jpeg',
-    '[6,33,22,67,887,22,4,7,89,8]'
+    'https://storage.googleapis.com/avzeus-dev/actresses/%E6%A9%8B%E6%9C%AC%E3%81%82%E3%82%8A%E3%81%AA.jpeg'
 );
 
 INSERT INTO actresses
 (
     name,
-    image_path,
-    vector
+    image_path
 )
 VALUES
 (
     '根尾あかり',
-    'https://storage.googleapis.com/avzeus-dev/actresses/%E6%A9%8B%E6%9C%AC%E3%81%82%E3%82%8A%E3%81%AA.jpeg',
-    '[6,33,22,67,887,22,4,7,89,8]'
+    'https://storage.googleapis.com/avzeus-dev/actresses/%E6%A9%8B%E6%9C%AC%E3%81%82%E3%82%8A%E3%81%AA.jpeg'
 );
 
 INSERT INTO actresses
 (
     name,
-    image_path,
-    vector
+    image_path
 )
 VALUES
 (
     '神宮寺ナオ',
-    'https://storage.googleapis.com/avzeus-dev/actresses/%E6%A9%8B%E6%9C%AC%E3%81%82%E3%82%8A%E3%81%AA.jpeg',
-    '[6,33,22,67,887,22,4,7,89,8]'
+    'https://storage.googleapis.com/avzeus-dev/actresses/%E6%A9%8B%E6%9C%AC%E3%81%82%E3%82%8A%E3%81%AA.jpeg'
 );
 
 INSERT INTO actresses
 (
     name,
-    image_path,
-    vector
+    image_path
 )
 VALUES
 (
     '奏音かのん',
-    'https://storage.googleapis.com/avzeus-dev/actresses/%E6%A9%8B%E6%9C%AC%E3%81%82%E3%82%8A%E3%81%AA.jpeg',
-    '[6,33,22,67,887,22,4,7,89,8]'
+    'https://storage.googleapis.com/avzeus-dev/actresses/%E6%A9%8B%E6%9C%AC%E3%81%82%E3%82%8A%E3%81%AA.jpeg'
 );
 
 INSERT INTO actresses
 (
     name,
-    image_path,
-    vector
+    image_path
 )
 VALUES
 (
     '奥田咲',
-    'https://storage.googleapis.com/avzeus-dev/actresses/%E6%A9%8B%E6%9C%AC%E3%81%82%E3%82%8A%E3%81%AA.jpeg',
-    '[6,33,22,67,887,22,4,7,89,8]'
+    'https://storage.googleapis.com/avzeus-dev/actresses/%E6%A9%8B%E6%9C%AC%E3%81%82%E3%82%8A%E3%81%AA.jpeg'
 );
 
 INSERT INTO actresses
 (
     name,
-    image_path,
-    vector
+    image_path
 )
 VALUES
 (
     '里美ゆりあ',
-    'https://storage.googleapis.com/avzeus-dev/actresses/%E6%A9%8B%E6%9C%AC%E3%81%82%E3%82%8A%E3%81%AA.jpeg',
-    '[6,33,22,67,887,22,4,7,89,8]'
+    'https://storage.googleapis.com/avzeus-dev/actresses/%E6%A9%8B%E6%9C%AC%E3%81%82%E3%82%8A%E3%81%AA.jpeg'
 );
 
 INSERT INTO actresses
 (
     name,
-    image_path,
-    vector
+    image_path
 )
 VALUES
 (
     '岬ななみ',
-    'https://storage.googleapis.com/avzeus-dev/actresses/%E6%A9%8B%E6%9C%AC%E3%81%82%E3%82%8A%E3%81%AA.jpeg',
-    '[6,33,22,67,887,22,4,7,89,8]'
+    'https://storage.googleapis.com/avzeus-dev/actresses/%E6%A9%8B%E6%9C%AC%E3%81%82%E3%82%8A%E3%81%AA.jpeg'
 );
 
 INSERT INTO actresses
 (
     name,
-    image_path,
-    vector
+    image_path
 )
 VALUES
 (
     '川上奈々美',
-    'https://storage.googleapis.com/avzeus-dev/actresses/%E6%A9%8B%E6%9C%AC%E3%81%82%E3%82%8A%E3%81%AA.jpeg',
-    '[6,33,22,67,887,22,4,7,89,8]'
+    'https://storage.googleapis.com/avzeus-dev/actresses/%E6%A9%8B%E6%9C%AC%E3%81%82%E3%82%8A%E3%81%AA.jpeg'
 );
