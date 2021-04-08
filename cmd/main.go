60d5ae4f8bfefb1ec2af4c153062885fe3580b90
souhub <ace0804sou@gmail.com>
2021-04-08 23:32:53 +0900

    Updated main.go

--- cmd/main.go
+++ cmd/main.go
@@ -13,9 +13,5 @@ func main() {
 	r.HandleFunc("/actresses", route.Actresses).Methods("GET")
 	r.HandleFunc("/wemen", route.Wemen).Methods("GET")
 	r.HandleFunc("/recommendation", route.GetRecommendationActresses).Methods("GET")
-	// r.HandleFunc("/selection", route.PostDataToAI).Methods("POST")
-	// r.HandleFunc("/outputted-data", route.GetDataFromAI).Methods("GET")
-	// r.HandleFunc("/data", route.GetDataFromAI).Methods("GET")
-	// r.HandleFunc("/recommendation", route.GetRecommendedActresses).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
